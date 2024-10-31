815fb94cc40e6f60a3b88f3320db78b68fe22f33
Satishneeli3004 <nsatishkumar95@gmail.com>
2024-10-31 09:28:39 +0000

    update configs

--- main.tf
+++ main.tf
@@ -5,19 +5,25 @@ provider "aws" {
 resource "aws_instance" "web" {
   ami           = "ami-0c55b159cbfafe1f0"  # amazon machine image id
   instance_type = "t2.micro"
-
+  key_name	= "my-key"
   tags = {
     Name = "web-server-instance"
   }
 }
 
-resource "aws_security_group" "web" {
+resource "aws_security_group" "web_sg" {
   name_prefix = "web-sg"
 
   ingress {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "ssh"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
